dc71c268c92c15be162c3393f9da826dafe16ebc
Przemyslaw Oskierko <p.oskierko@gmail.com>
2018-08-25 11:56:15 +0200

    Created simple csv file generator

---
+++ csvGenerator.py
@@ -0,0 +1,25 @@
+import sys
+from random import randint
+import csv
+
+assert len(sys.argv) == 5, "You need to pass 4 parameters: \n1)filename \n2)number of tests \n3)max duration \n4)number of runs"
+
+filename  = sys.argv[1]
+num_of_tests, max_duration, num_of_runs = map(int, sys.argv[2:])
+
+print ("filename %s\n num_of_tests %d\n max_duration %d\n num_of_runs %d\n" % (filename, num_of_tests, max_duration, num_of_runs))
+
+def genTestId():
+	return randint(0, num_of_tests)
+
+def genDuration():
+	return randint(1, max_duration)
+
+labels = ["Test ID", "Duration"]
+writer = csv.DictWriter(open(filename, "w+"), fieldnames = labels)
+
+for i in range(num_of_runs):
+	writer.writerow(dict([
+		(labels[0], genTestId()),
+		(labels[1], genDuration())
+	]))
